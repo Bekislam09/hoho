9c2906f93ac864e5f733f42090877dc91e383c88
Bekislam09 <email@example.com>
2025-03-05 13:29:54 +0600

    f

--- src/app/routes/App.jsx
+++ src/app/routes/App.jsx
@@ -1,84 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../styles/app.scss';
 import { 
-  HomePage,
-  AfishaPage,
-  AfishaDetail,
-  ServicesPage,
-  ProjectPage,
-  ProjectDetail,
-  NewsPage,
-  NewsDetail,
-  CatalogPage,
-  ReaderPage,
   AboutPage,
-  SupportPage,
-  ElectronicPage,
-  ElectronicDetail,
-  ProfessionalPage
 } from '../../pages'; 
 import { Footer, Header, Breadcrumb } from '../../widgets';
 
 const routesArr = [
   {
     path: '/',
-    element: <HomePage />
-  },
-  {
-    path: '/afisha',
-    element: <AfishaPage />
-  },
-  {
-    path: '/afisha-detail/:id',
-    element: <AfishaDetail />
-  },
-  {
-    path: '/services',
-    element: <ServicesPage />
-  },
-  {
-    path: '/project',
-    element: <ProjectPage />
-  },
-  {
-    path: '/project-detail/:id',
-    element: <ProjectDetail />
-  },
-  {
-    path: '/news',
-    element: <NewsPage />
-  },
-  {
-    path: '/news-detail/:id',
-    element: <NewsDetail />
-  },
-  {
-    path: '/catalog',
-    element: <CatalogPage />
-  },
-  {
-    path: '/reader',
-    element: <ReaderPage />
-  },
-  {
-    path: '/about',
     element: <AboutPage />
-  },
-  {
-    path: '/support',
-    element: <SupportPage />
-  },
-  {
-    path: '/electronic',
-    element: <ElectronicPage />
-  },
-  {
-    path: '/electronic-detail/:id',
-    element: <ElectronicDetail />
-  },
-  {
-    path: '/professional',
-    element: <ProfessionalPage />
   },
 ];
 
